4ea3744d8d76cc8f47bc6985cfe623fb3a5bb0b2
Ronie Salgado <roniesalg@gmail.com>
2015-11-25 03:40:13 -0300

    I added some missing shaders.

---
+++ data/shaders/canvas2d/colorVertex.hlsl
@@ -0,0 +1,26 @@
+cbuffer TransformationBuffer : register(b0)
+{
+    matrix projectionMatrix;
+    matrix modelMatrix;
+    matrix viewMatrix;
+}
+
+struct VertexInput
+{
+    float4 position : A;
+    float4 color : C;
+};
+
+struct VertexOutput
+{
+    float4 color : COLOR;
+    float4 position : SV_POSITION;
+};
+
+VertexOutput main(VertexInput input)
+{
+    VertexOutput output;
+    output.color = input.color;
+    output.position = mul(projectionMatrix, mul(viewMatrix, mul(modelMatrix, input.position)));
+    return output;
+}
